5b21540c55715c1433e12e1f875a0fbaf4658f33
phongphanq089 <phongphanq089@gmail.com>
2022-10-04 13:23:55 +0700

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./app.scss";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Movies from "./page/Movies/Movies";
+import Search from "./page/Movies/Movies";
+import Trending from "./page/Trendding/Trending";
+import TVshow from "./page/TVshow/TVshow";
+import Header from "./compoment/Header/Header";
+import Footer from "./compoment/Footer/Footer";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Trending />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tv" element={<TVshow />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/compoment/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="navbar-menu">
+                <h4>Trending</h4>
+                <h4>Movie</h4>
+                <h4>TVmovies</h4>
+                <h4>Search</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Header
